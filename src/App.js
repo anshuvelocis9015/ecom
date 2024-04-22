77e71cfd6627597fc46f6a9c4e2890e32f0ce57a
anshu9015 <anshu.kmr9015@gmail.com>
2024-04-22 15:49:07 +0530

    home page build

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 // import logo from './logo.svg';
 import './App.css';
+import Home from './components/Pages/Home';
 
 function App() {
   return (
     <div className="App">
       <h1>Ecommerce website</h1>
+      <Home />
     </div>
   );
 }
